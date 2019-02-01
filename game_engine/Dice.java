789cc5124c40871634ee286e4bdc08d90f81e466
Bryan Sng <sngby98@gmail.com>
2019-02-01 16:55:18 +0000

    Added Dice and Dices classes

---
+++ game_engine/Dice.java
@@ -0,0 +1,70 @@
+package game_engine;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * This class represents the Dice object in Backgammon from the UI POV.
+ * @author Bryan Sng
+ * @email sngby98@gmail.com
+ *
+ */
+public class Dice extends ImageView {
+	private final int MAX_DICE_SIZE = 6;
+	private Image[] dices;
+	
+	/**
+	 * Default Constructor
+	 * 		- Initialize the dices array with the possible dice images (i.e. 1-6).
+	 * @param colour
+	 */
+	public Dice(String colour) {
+		super();
+		dices = new Image[MAX_DICE_SIZE];
+		initImages(colour);
+	}
+	
+	/**
+	 * Initializes the dice images:
+	 * 		- by getting the images from file,
+	 * 		- adding them to the dices array.
+	 * @param colour
+	 */
+	private void initImages(String colour) {
+		FileInputStream input = null;
+		colour.toLowerCase();
+		try {
+			for (int i = 0; i < dices.length; i++) {
+				input = new FileInputStream("src/img/dices/" + colour + "/" + (i+1) + ".png");
+				dices[i] = new Image(input);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Set the image of dice based on result.
+	 * i.e. If result is 1, show image with dice at 1.
+	 * @param result of rolling the dice.
+	 */
+	private void draw(int result) {
+		setImage(dices[result-1]);
+	}
+	
+	/**
+	 * Get the roll dice result (i.e. number from 1 to 6),
+	 * draw the dice with the result,
+	 * return the result.
+	 * @return the result of rolling the dice.
+	 */
+	public int roll() {
+		Random rand = new Random();
+		int res = rand.nextInt(MAX_DICE_SIZE) + 1;
+		draw(res);
+		return res;
+	}
+}
---
+++ game_engine/Dices.java
@@ -0,0 +1,74 @@
+package game_engine;
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+
+/**
+ * This class represents a HBox of dices.
+ * @author Bryan Sng
+ * @email sngby98@gmail.com
+ *
+ */
+public class Dices extends HBox {
+	private Dice[] dices;
+	
+	/**
+	 * Default Constructor
+	 * 		- Initialize the dices array with two dices.
+	 * @param colour of dices
+	 */
+	public Dices(String colour) {
+		this(colour, 2);
+	}
+	
+	/**
+	 * Overloaded Constructor
+	 * 		- Initialize the dices array with any number of dices.
+	 * 		- Set this node's alignment.
+	 * @param colour of dices.
+	 * @param numberOfDices number of dices. 
+	 */
+	public Dices(String colour, int numberOfDices) {
+		super();
+		dices = new Dice[numberOfDices];
+		initDices(colour);
+		setAlignment(Pos.CENTER);
+	}
+	
+	/**
+	 * Initialize the individual dices and assign them into the dices array.
+	 * @param colour of dices.
+	 */
+	private void initDices(String colour) {
+		for (int i = 0; i < dices.length; i++) {
+			dices[i] = new Dice(colour);
+			getChildren().add(dices[i]);
+		}
+	}
+	
+	/**
+	 * Return an array of integers, containing the result of each dice roll.
+	 * @return result of each dice roll in terms of an array of integers.
+	 */
+	public int[] getTotalRoll() {
+		int[] res = new int[dices.length];
+		for (int i = 0; i < dices.length; i++) {
+			res[i] = dices[i].roll();
+		}
+		return res;
+	}
+	
+	/* IGNORE FIRST, NOT PART OF SPRINT SCOPE.
+	 * This is considered for handling doubling cubes.
+	// upon calling, this method should double current cube objects in dices.
+	public void doubleCube() {
+		
+	}
+	
+	// upon calling, removes the double cube instances (i.e. set them to null).
+	// and remove them from the nodes.
+	public void removeDoubleCube() {
+		
+	}
+	*/
+}
