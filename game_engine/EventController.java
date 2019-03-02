1b23e055f137577f62c623a9fe20991ded80371c
Braddy <braddy.yeoh@ucdconnect.ie>
2019-03-02 18:39:14 +0000

    [Refactor] Trimmed inputted text to enable user to enter move even with leading or trailing spaces in the input. Changed the string to be checked from String args[0] to String text

--- game_engine/EventController.java
+++ game_engine/EventController.java
@@ -182,8 +182,8 @@ public class EventController implements ColorParser, InputValidator {
 				cmd.runCommand("/next");
 			} else if (text.trim().isEmpty()) {
 				// ignores if string empty or whitespace only.
-			} else if (gameplay.isMapped() && gameplay.isKey(args[0].toUpperCase())) {
-				cmd.runCommand(gameplay.getMapping(args[0].toUpperCase()));
+			} else if (gameplay.isMapped() && gameplay.isKey(text.toUpperCase().trim())) {
+				cmd.runCommand(gameplay.getMapping(text.toUpperCase().trim()));
 			} else {
 				// player chat, need to implement players to differentiate which player is
 				// which.
